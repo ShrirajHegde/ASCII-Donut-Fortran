a5b97c893b2fa3f363800f72e67e6122f20b8181
Shriraj Hegde <shriraj.hegde@gmail.com>
2021-10-28 19:15:25 +0530

    Rename Variables

--- donut.f90
+++ donut.f90
@@ -1,67 +1,68 @@
 program donut
     implicit none
     INTEGER::k
-    real,PARAMETER::scale=1
-    real,PARAMETER::speed=1
+    real,PARAMETER::scale=3
+    real,PARAMETER::speed=0.4
     INTEGER,PARAMETER::height=scale*80
     INTEGER,PARAMETER::width=scale*22
-    real::a=0,b=0,j,i,z(width*height)
+    real::angle_x=0,angle_y=0,theta,phi,z_buffer(width*height)
     CHARACTER:: screen(width*height)
     real,PARAMETER::TAU=3.14159*2
-    CHARACTER:: ATABLE*12=".,-~:;=!*#$@"
-    real::sini,cosj,sina,sinj,cosa,cosj2,mess
-    real::cosi,cosb,t,sinb
-    INTEGER::x,y,o,N
+    CHARACTER(len=12):: ATABLE=".,-~:;=!*#$@"
+    real::sinphi,costheta,sinax,sintheta,cosax,cosjthetap2,mess
+    real::cosphi,cosay,t,sinay
+    INTEGER::x,y,o,lum
     CHARACTER::ASCII10=achar(10)
     CHARACTER::ESC=achar(27)
 
     write(*,"(A)",advance="no")ESC//"[2J"
 
     do while(.true.)
-        z=0
+        z_buffer=0.0
         screen=" "
-        j=0
-        do while(j<TAU)
-            i=0
-            do while(i<TAU)
-                sini=sin(i)
-                cosj=cos(j)
-                cosj2=cosj+2
-                sina=sin(a)
-                sinj=sin(j)
-                cosa=cos(a)
-                cosi=cos(i)
-                cosb=cos(b)
-                sinb=sin(b)
-                mess=1.00/(sini*cosj2*sinA+sinj*cosA+5)
-                t=sini*cosj2*cosa - sinj*sina;
-                x=int(40*scale+scale*30*mess*(cosi*cosj2*cosB-t*sinB))
-                y=int(scale*12+scale*15*mess*(cosi*cosj2*sinB +t*cosB))
+        theta=0.0
+        do while(theta<TAU)
+            phi=0.0
+            do while(phi<TAU)
+                sinphi=sin(phi)
+                costheta=cos(theta)
+                cosjthetap2=costheta+2
+                sinax=sin(angle_x)
+                sintheta=sin(theta)
+                cosax=cos(angle_x)
+                cosphi=cos(phi)
+                cosay=cos(angle_y)
+                sinay=sin(angle_y)
+                mess=1.00/(sinphi*cosjthetap2*sinax+sintheta*cosax+5)
+                t=sinphi*cosjthetap2*cosax - sintheta*sinax;
+                x=int(40*scale+scale*30*mess*(cosphi*cosjthetap2*cosay-t*sinay))
+                y=int(scale*12+scale*15*mess*(cosphi*cosjthetap2*sinay+t*cosay))
                 o=x+height*y
-                N=int(8*((sinj*sina-sini*cosj*cosa)*cosb-sini*cosj*sina-sinj*cosa-cosi *cosj*sinb))
-                if(width>y .and. y>0 .and. x>0 .and. height>x .and. mess>z(o))then
-                    z(o)=mess
-                    if (N>0)then
-                        screen(o)=ATABLE(N:N) 
+                lum=int(8*((sintheta*sinax-sinphi*costheta*cosax)* &
+                        cosay-sinphi*costheta*sinax-sintheta*cosax-cosphi *costheta*sinay))
+                if(width>y .and. y>0 .and. x>0 .and. height>x .and. mess>z_buffer(o))then
+                    z_buffer(o)=mess
+                    if (lum>0)then
+                        screen(o)=ATABLE(lum:lum) 
                     else
                         screen(o)="."
                     endif
                 endif
-                i=i+0.02
+                phi=phi+0.02
             enddo
-            j=j+0.07
+            theta=theta+0.07
         enddo
 
         write(*,"(A)",advance="no")ESC//"[d"
         
-        do k=0,width*height+1
+        do k=1,width*height
             if(mod(k,height)==0)then
                 write(*,"(A)",advance="no")ASCII10
             else
                 write(*,"(A)",advance="no")screen(k)
             endif
         enddo
-        a=a+0.04*speed
-        b=b+0.04*speed
+        angle_x=angle_x+0.04*speed
+        angle_y=angle_y+0.04*speed
     enddo
 end program donut
